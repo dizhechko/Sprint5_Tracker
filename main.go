bbfe1709c65417b7991904be90599378d4d6e069
Dizhechko Nataly <dizhechko.nata@yandex.ru>
2024-04-24 10:37:28 +0500

    second release

--- main.go
+++ main.go
@@ -34,6 +34,9 @@ func (t Training) distance() float64 {
 // meanSpeed возвращает среднюю скорость бега или ходьбы.
 func (t Training) meanSpeed() float64 {
 	// вставьте ваш код ниже
+	if t.Duration.Hours() == 0 {
+		return 0
+	}
 	return (float64(t.Action) * (t.LenStep / float64(MInKm))) / t.Duration.Hours()
 }
 
@@ -178,7 +181,7 @@ type Swimming struct {
 // Это переопределенный метод Calories() из Training.
 func (s Swimming) meanSpeed() float64 {
 	// вставьте ваш код ниже
-	if s.LengthPool == 0 {
+	if s.Training.Duration.Hours() == 0 {
 		return 0
 	}
 	return float64(s.LengthPool) * float64(s.CountPool) / MInKm / float64(s.Training.Duration.Hours())
